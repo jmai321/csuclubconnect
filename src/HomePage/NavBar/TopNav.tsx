ddae33c6acf90e06c86f2ba497631c3056e38ccd
Jonathan  Mai <jonathannmai@gmail.com>
2023-10-10 19:31:21 -0700

    routed home buttons to redirect to homepage

--- src/HomePage/NavBar/TopNav.tsx
+++ src/HomePage/NavBar/TopNav.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopNav: React.FC = () => {
     return ( 
         <div className="flex p-0">
-            <button className="border-l-2 border-l-solid p-3 border-l-custom-color3 hover:bg-custom-color2">Home</button>
+            <Link to ="/" className="border-l-2 border-l-solid p-3 border-l-custom-color3 hover:bg-custom-color2">Home</Link>
             <button className="border-l-2 border-l-solid p-3 border-l-custom-color3 hover:bg-custom-color2">Dashboard</button>
             <button className="border-l-2 border-l-solid p-3 border-l-custom-color3 hover:bg-custom-color2">Notif</button>
             <button className="border-l-2 border-l-solid p-3 border-l-custom-color3 hover:bg-custom-color2">User</button>
--- src/SchoolPage/SchoolTopNav.tsx
+++ src/SchoolPage/SchoolTopNav.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SchoolTopNav: React.FC = () => {
     return (
         <div className="flex absolute top-0 right-0 p-0 z-10">
-            <button className="border-l-2 border-b-2 border-solid p-3 border-custom-color3 hover:bg-custom-color2">Home</button>
-            <button className="border-l-2 border-b-2 border-solid p-3 border-custom-color3 hover:bg-custom-color2">Dashboard</button>
+            <Link to="/" className="border-l-2 border-b-2 border-solid p-3 border-custom-color3 hover:bg-custom-color2">Home</Link>
+            <button className="border-l-2 border-b-2 border-solid p-3 border-custom-color3 hover:bg-custom-color2">Dashboard </button>
             <button className="border-l-2 border-b-2 border-solid p-3 border-custom-color3 hover:bg-custom-color2">Notif</button>
             <button className="border-l-2 border-b-2 border-solid p-3 border-custom-color3 hover:bg-custom-color2">User</button>
         </div>
