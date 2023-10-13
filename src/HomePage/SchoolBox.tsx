b77d8e666b4692661c4d420cb63e708b4a9e95ff
Jonathan  Mai <jonathannmai@gmail.com>
2023-10-12 22:17:53 -0700

    updated schoolboxprops

--- src/HomePage/SchoolBox.tsx
+++ src/HomePage/SchoolBox.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-interface SchoolBoxProps {
-  name: string;
-}
-
-const SchoolBox: React.FC<SchoolBoxProps> = ({ name }) => {
+const SchoolBox: React.FC<{name: string}> = ({ name }) => {
   return (
     <button className="bg-gray-200 hover:bg-gray-300 mx-5 my-5 w-60 h-48 flex items-center justify-center rounded-lg">
       <h2 className="text-xl font-bold">{name}</h2>
